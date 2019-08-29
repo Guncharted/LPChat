5ecedf43287f26de46d10263d3e2dddae748c0eb
Oleg Gonchar <hanchar.aleh@live.com>
2019-08-29 21:23:14 +0300

    added pass confirm field

--- LPChat.Core/DTO/UserForRegister.cs
+++ LPChat.Core/DTO/UserForRegister.cs
@@ -13,5 +13,8 @@ namespace LPChat.Core.DTO
         [Required]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "Пароль должен быть от 8 до 20 знаков")]
         public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
