7a98c97543854cb0547a6f9c7c57b19d11e8ca84
Oleg Gonchar <hanchar.aleh@live.com>
2019-09-16 19:42:53 +0300

    Generic repository and repo manager interfaces

---
+++ LPChat.Core/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using LPChat.Domain.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPChat.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync(T item);
+        Task<T> FindById(Guid id);
+        Task<IEnumerable<T>> GetAsync();
+        Task<IEnumerable<T>> GetAsync(Func<T, bool> predicate);
+        Task RemoveAsync(T item);
+        Task UpdateAsync(T item);
+        Task UpdateRangeAsync(IEnumerable<T> items);
+    }
+}
---
+++ LPChat.Core/Interfaces/IRepositoryManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LPChat.Domain.Interfaces
+{
+    public interface IRepositoryManager
+    {
+        IRepository<T> GetRepository<T>() where T : class;
+    }
+}
