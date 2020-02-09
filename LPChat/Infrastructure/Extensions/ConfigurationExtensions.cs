40851f43c2fa5c44d284fafee57307502579f4b8
Oleg Gonchar <hanchar.aleh@live.com>
2020-02-09 15:56:40 +0300

    added swagger back

--- LPChat/Infrastructure/Extensions/ConfigurationExtensions.cs
+++ LPChat/Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -1,4 +1,6 @@
-﻿using LPChat.Infrastructure.Interfaces;
+﻿using AutoMapper;
+using LPChat.Infrastructure.Interfaces;
+using LPChat.Infrastructure.Mapping;
 using LPChat.Infrastructure.Services;
 using LPChat.Middleware;
 using LPChat.MongoDb;
@@ -8,60 +10,72 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.IO;
 using System.Reflection;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace LPChat.Infrastructure
 {
-	public static class ConfigurationExtensions
-	{
-		public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
-		{
-			services.AddSingleton<IMessageService, MessageService>();
-			services.AddScoped<IAuthService, AuthService>();
-			services.AddScoped<IChatService, ChatService>();
-			services.AddScoped<IPersonInfoService, PersonInfoService>();
+    public static class ConfigurationExtensions
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IMessageService, MessageService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IChatService, ChatService>();
+            services.AddScoped<IPersonInfoService, PersonInfoService>();
 
-			services
-				.AddSingleton<IRepositoryManager, MongoReposiotoryManager>(x => new MongoReposiotoryManager("cchat", configuration.GetConnectionString("MongoLocal")));
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AllowNullCollections = true;
+                cfg.AddProfile<MainProfile>();
+            });
 
-			services.AddCors();
+            services
+                .AddSingleton<IRepositoryManager, MongoReposiotoryManager>(x => new MongoReposiotoryManager("cchat", configuration.GetConnectionString("MongoLocal")));
+
+            services.AddCors();
             services.AddMemoryCache();
 
-			//services.AddSwaggerGen(c =>
-			//{
-			//	c.SwaggerDoc("v1", Microsoft.OpenApi.ope { Title = "LP Chat", Version = "v1" });
-			//	c.DescribeAllEnumsAsStrings();
-			//	c.DocInclusionPredicate((doc, api) => true);
-			//	c.CustomSchemaIds(x => x.FullName);
-			//	var xmlDocFile = Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
-			//	if (File.Exists(xmlDocFile))
-			//	{
-			//		c.IncludeXmlComments(xmlDocFile);
-			//	}
-			//});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "LP Chat", Version = "v1" });
+                c.DocInclusionPredicate((doc, api) => true);
+                c.CustomSchemaIds(x => x.FullName);
+                var xmlDocFile = Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+                if (File.Exists(xmlDocFile))
+                {
+                    c.IncludeXmlComments(xmlDocFile);
+                }
+            });
 
-			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddJwtBearer(options =>
-				{
-					options.TokenValidationParameters = new TokenValidationParameters
-					{
-						ValidateIssuerSigningKey = true,
-						IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration.GetSection("AppSettings:Token").Value)),
-						ValidateIssuer = false,
-						ValidateAudience = false
-					};
-				});
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
 
-			services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-		}
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
 
-		public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
-		{
-			return builder.UseMiddleware<ExceptionHandler>();
-		}
-	}
+            services.AddMvc(options => options.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                });
+        }
+
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandler>();
+        }
+    }
 }
